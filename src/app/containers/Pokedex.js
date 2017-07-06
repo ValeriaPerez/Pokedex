90851f9cfe43a756e58860c8a028604873a3f7d0
ValeriaPerez <valeria.pjaimes@gmail.com>
2017-07-06 00:51:56 -0500

    Second advance

--- src/app/containers/Pokedex.js
+++ src/app/containers/Pokedex.js
@@ -1,10 +1,45 @@
 import React, {Component} from 'react';
+import Card from './../components/Card';
+import axios from 'axios';
 
 export default class Pokedex extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			list: [],
+			loading: true
+		}
+	}
+
+	componentDidMount() {
+    axios.get(`https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json`)
+      .then(function (response) {
+        list: response.obj.data
+        loading: false
+		    console.log(response);
+		  })
+		  .catch(function (error) {
+		  	loading: true
+		  });
+  }
+
   render() {
     return (
-      <div>
-        Pokedex
+      <div style={{textAlign: 'center', backgroundColor: 'red', margin: '0', padding: '0', height: '100%',}}>
+      	<img src="./app/logo.png" style={{maxWidth: '60%', margin: '30px',}} title="Logo Pokemon" alt="Logo" />
+        <div>
+        	{this.state.loading ? 
+        		<img src="https://media.giphy.com/media/rFrMBUUuKs22Y/giphy.gif"/> 
+        	: null}
+          {this.state.list.map((obj, index) => {
+           return <Card
+             index={index}
+             key={index}
+             image={this.props.image}
+             type={type}
+             />;
+          })}
+         </div>
       </div>
     );
   }

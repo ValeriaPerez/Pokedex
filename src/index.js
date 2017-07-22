b17a75b904dc90ea577a8c3ef1341ae6b4a6d9b2
ValeriaPerez <valeria.pjaimes@gmail.com>
2017-07-22 13:24:01 -0500

    Add axios, actions, reducers, middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import Pokedex from './app/containers/Pokedex';
+import Valeria from './app/containers/Valeria';
 import PokedexBuild from './app/containers/PokedexBuild';
 import configureStore from './app/store/configureStore';
 import {Router, Route, browserHistory} from 'react-router';
@@ -15,7 +16,8 @@ const store = configureStore();
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={PokedexBuild}/>
+      <Route path="/" component={Valeria}/>
+      <Route path="/poke" component={PokedexBuild}/>
       <Route path="/pokedex" component={Pokedex}/>
     </Router>
   </Provider>,
